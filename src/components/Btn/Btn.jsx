34bca4317cb615680e389f12ebfe596b58d3412b
kwinkich <vakord21@gmail.com>
2023-06-01 18:42:19 +0300

    Fist commit

---
+++ src/components/Btn/Btn.jsx
@@ -0,0 +1,11 @@
+import styles from './Btn.module.css'
+
+function Button() {
+	return(
+		<a href="#projects" className={styles.btn}>
+			Преглянути проєкти
+		</a>
+	)
+}
+
+export default Button;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import Logo from '../Logo/Logo.jsx'
+import styles from './Header.module.css'
+import ukr from '../../assets/ukr.png'
+import en from '../../assets/en.png'
+import '../../index.css'
+
+function Header() {
+	return(
+		<header className={styles.header}>
+			<div className={styles.container}>
+				<div className={styles.content}>
+					<Logo />
+					<nav className={styles.nav}>
+						<ul>
+							<li><a href="#hero">Головна</a></li>
+							<li><a href="#about">Про мене</a></li>
+							<li><a href="#projects">Проєкти</a></li>
+							<li><a href="#skills">Навички</a></li>
+							<li><a href="#media">Контакти</a></li>
+						</ul>
+					</nav>
+					<div className={styles.lang}>
+						<p>Мова:</p>
+						<button><img src={ukr} alt="" /></button>
+						<button><img src={en} alt="" /></button>
+					</div>	
+					<div className={styles.burger}>
+						<span></span>
+					</div>
+				</div>
+			</div>
+		</header>
+	)
+}
+
+export default Header
